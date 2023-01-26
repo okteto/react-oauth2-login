fdfe96f71ac65a871c1b98b56dda2718a88a8499
Tiago Ferreira <tiago@okteto.com>
2023-01-26 11:20:28 +0100

    test: update test and snapshots

--- src/LoginOAuth2.test.js
+++ src/LoginOAuth2.test.js
@@ -1,68 +1,92 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
+
+import { fireEvent, render } from '@testing-library/react';
+import { findInstance } from 'react-dom-instance';
 
 import LoginOAuth2 from './LoginOAuth2.js';
 
-test('Renders defaults', () => {
-  const component = renderer.create(
-    <LoginOAuth2
-      clientId="foo"
-      redirectUri="http://foo.test/auth/github"
-      authorizeUri="http://bar.test"
-    />
-  );
-  let tree = component.toJSON();
-
-  expect(tree).toMatchSnapshot();
-});
-
-test('Renders with `className`', () => {
-  const component = renderer.create(
-    <LoginOAuth2
-      clientId="foo"
-      redirectUri="http://foo.test/auth/github"
-      authorizeUri="http://bar.test"
-      className="foobar"
-    />
-  );
-  let tree = component.toJSON();
-
-  expect(tree).toMatchSnapshot();
-});
-
-test('Renders with `buttonText`', () => {
-  const component = renderer.create(
-    <LoginOAuth2 clientId="foo"
-      authorizeUri="http://bar.test"
-      redirectUri="http://foo.test/auth/github"
-      buttonText="Foo"
-    />
-  );
-  let tree = component.toJSON();
-
-  expect(tree).toMatchSnapshot();
-});
-
-test('Opens OAuth dialog', () => {
-  const clientId = 'foo';
-  const redirectUri = 'http://foo.test/auth/github';
-
-  const component = (
-    <LoginOAuth2
-      clientId={clientId}
-      redirectUri={redirectUri}
-      state="hello"
-      authorizeUri="http://bar.test"
-    />
-  );
-  const wrapper = shallow(component);
-
-  wrapper.find('button').simulate('click');
-
-  const query = `client_id=${clientId}&scope=email&redirect_uri=${redirectUri}&response_type=code&state=hello`
-
-  expect(wrapper.instance().popup.url).toBe(
-    `http://bar.test?${query}`
-  );
-});
+describe('LoginOauth2', () => {
+  it('should render defaults', () => {
+    const { container, getByRole } = render(
+      <LoginOAuth2
+        clientId="foo"
+        redirectUri="http://foo.test/auth/github"
+        authorizeUri="http://bar.test"
+      />
+    );
+    
+    expect(container.firstChild).toMatchSnapshot();
+  
+    const button = getByRole('button', {
+      name: 'Sign in',
+      hidden: false,
+    });
+  
+    expect(button).toBeEnabled;
+    expect(button.getAttribute('class')).toBe('');
+  });
+  
+  it('should render with `className`', () => {
+    const { container, getByRole } = render(
+      <LoginOAuth2
+        clientId="foo"
+        redirectUri="http://foo.test/auth/github"
+        authorizeUri="http://bar.test"
+        className="foobar"
+      />
+    );
+  
+    const button = getByRole('button', {
+      name: 'Sign in',
+      hidden: false,
+    });
+  
+    expect(button.getAttribute('class')).toBe('foobar');
+  });
+  
+  it('should render with `buttonText`', () => {
+    const buttonText = 'Foo';
+  
+    const { container, getByRole } = render(
+      <LoginOAuth2 clientId="foo"
+        authorizeUri="http://bar.test"
+        redirectUri="http://foo.test/auth/github"
+        buttonText={buttonText}
+      />
+    );
+  
+    const button = getByRole('button', {
+      name: buttonText,
+      hidden: false,
+    });
+  
+    expect(button).toBeEnabled;
+    expect(button.getAttribute('class')).toBe('');
+  });
+  
+  it('should open OAuth dialog', async () => {
+    const clientId = 'foo';
+    const redirectUri = 'http://foo.test/auth/github';
+    const authorizeUri = 'http://bar.test'
+    const query = `client_id=${clientId}&scope=email&redirect_uri=${redirectUri}&response_type=code&state=hello`
+  
+    const { container, getByRole } = render(
+      <LoginOAuth2
+        clientId={clientId}
+        redirectUri={redirectUri}
+        state="hello"
+        authorizeUri={authorizeUri}
+      />
+    );
+  
+    const button = getByRole('button', {
+      name: 'Sign in'
+    });
+  
+    await fireEvent.click(button)
+  
+    expect(findInstance(container).popup.url).toBe(
+      `${authorizeUri}?${query}`
+    );
+  });
+})
