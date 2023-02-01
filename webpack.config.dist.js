5b43e7d3f6f9b6346baef49707b626a2a6d334c4
Tiago Ferreira <tiago@okteto.com>
2023-02-01 10:56:51 +0100

    chore: remove react-dom from peerDep and webpack external

--- webpack.config.dist.js
+++ webpack.config.dist.js
@@ -5,38 +5,37 @@ module.exports = {
   mode: 'production',
   devtool: 'cheap-module-source-map',
   entry: [
-    './src/index.js'
+    './src/index.js',
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'index.js',
     libraryTarget: 'umd',
-    library: 'LoginOAuth2'
+    library: 'LoginOAuth2',
   },
   module: {
     rules: [{
       use: 'babel-loader',
       test: /\.js$/,
-      exclude: /node_modules/
+      exclude: /node_modules/,
     }],
   },
   externals: {
-    'react': 'react',
-    'react-dom': 'ReactDOM'
+    react: 'react',
   },
   resolve: {
-    extensions: ['.js']
+    extensions: ['.js'],
   },
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify('production')
+        NODE_ENV: JSON.stringify('production'),
       },
     }),
     new webpack.LoaderOptionsPlugin({
       minimize: true,
-      debug: false
+      debug: false,
     }),
-    new webpack.optimize.AggressiveMergingPlugin()
-  ]
+    new webpack.optimize.AggressiveMergingPlugin(),
+  ],
 };
